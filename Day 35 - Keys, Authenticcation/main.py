fcbf078500a0915963fb870f631c2428ee37df57
NarayanIyer25 <88261924+NarayanIyer25@users.noreply.github.com>
2023-09-18 16:23:32 +0530

    Add files via upload

---
+++ Day 35 - Keys, Authenticcation/main.py
@@ -0,0 +1,35 @@
+
+import requests
+from twilio.rest import Client
+
+
+client = Client(account_sid, auth_token)
+
+
+
+OWM_ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall?'
+parameter = {
+    'lat': 51.507351,
+    'lon': -0.127758,
+    'exclude': "current,minutely,daily"
+}
+response = requests.get(OWM_ENDPOINT,params=parameter)
+print(response.json())
+hour_data = response.json()['hourly']
+weather_data = []
+for i in range(len(hour_data)):
+    if i<=12:
+        weather_data.append(hour_data[i]['weather'])
+boolean = False
+for i in weather_data:
+    value = i[0]['id']
+    if int(value)<700:
+        boolean = True
+val = ''
+if boolean:
+    val = "Bring Your Umbrella"
+message = client.messages.create(
+  from_='+12565307340',
+  to='+919912747134',
+  body= val
+)
