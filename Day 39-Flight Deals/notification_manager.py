5137f38199a82543b38a044bbb729d5f4e78088a
NarayanIyer25 <88261924+NarayanIyer25@users.noreply.github.com>
2023-10-06 22:22:45 +0530

    Update notification_manager.py

--- Day 39-Flight Deals/notification_manager.py
+++ Day 39-Flight Deals/notification_manager.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def __init__(self):
-        self.cliend_id= 'AC18af72981bade760928c2cd8b3181623'
-        self.token = 'c9f69878481dd75b589019225a676180'
+        self.cliend_id= ''
+        self.token = ''
         self.client = Client(self.cliend_id,self.token)
 
     def send_message(self,price,from_city,to_city,from_date,to_date):
